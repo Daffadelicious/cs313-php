e10ec4d7744548c1ab1761c5e219c95fb5f6ea1b
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2021-01-16 21:17:49 -0800

    added files for homepage

---
+++ web/home_page/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BriAnna Marsh's Home Page | CS 313</title>
+</head>
+<body>
+    <header>
+        <img src="#" alt="placeholder">
+        <?php echo "The current date is: " . date("j F, yy"); ?> 
+    </header>
+    
+</body>
+</html>
