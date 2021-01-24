13c646a82c64cd1fa0f3a3427ac78628e3e55d30
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2021-01-23 22:37:36 -0800

    Added files

---
+++ web/week_03/Shopping_cart/index.php.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Daffadelicious | Shopping </title>
+</head>
+<body>
+    <h1>Welcome to Daffadelicious</h1>
+    <p>A shop for delicious flowers. Perfect for feeding deer, rabbits, tortises, and more! </p>
+
+    <h2>Available during spring time</h2>
+    <section class="springSelection">
+        <div class="row">
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+        </div>
+        <div class="row">
+        <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+        </div>
+        <div class="row">
+        <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <figure>
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+        </div>
+    </section>
+</body>
+</html>
